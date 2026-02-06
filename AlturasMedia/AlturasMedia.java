bae6b1e7e20fef2a7966d66be03f92b6fb4e92dd
Pedro Antonio <pedroantoniolopesdearruda6@gmail.com>
2026-02-06 15:50:27 -0300

    Adicionando Projetos/Exercicios

---
+++ AlturasMedia/AlturasMedia.java
@@ -0,0 +1,35 @@
+package AlturasMedia;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AlturasMedia {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Quantas alturas quer digitar?");
+        int n = sc.nextInt();
+
+        double[] vetor = new double[n];
+
+        for (int i = 0; i < n; i++){
+            vetor[i] = sc.nextDouble();
+        }
+
+        double soma = 0;
+
+        for (int i = 0; i < n; i++){
+            soma += vetor[i];
+        }
+
+        double media = soma/n;
+
+        System.out.println("Media: "+media);
+
+
+
+
+        sc.close();
+    }
+}
